253c7f1a228aa9b2c4781b01dc189025423db645
Kenneth <kennethalisen433@protonmail.com>
2024-01-23 14:56:12 +0800

    added configure logging function, used pyexiv2.ImageData to get metadata in ImageInfo class.

--- codes/image_viewer.py
+++ codes/image_viewer.py
@@ -2,9 +2,12 @@ import re
 import time
 import logging
 import webbrowser
+import tkinter as tk
 from pathlib import Path
 from datetime import date
-from tkinter import Tk, Label, Button, filedialog
+from tkinter import filedialog
+
+import ttkbootstrap as ttk
 
 import pyexiv2
 from gmplot import gmplot
@@ -13,19 +16,26 @@ from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 
 
-def is_image(file):
-    file = Path(file).absolute()
-    return file.is_file() and file.suffix.lower() in ('.jpg', 'jpeg', 'png')
+def configure_logging(*, log_level=logging.ERROR, log_file=None):
+    log_handlers = [logging.StreamHandler()]
+    if log_file is not None:
+        log_handlers = [logging.FileHandler(log_file, mode='w')]
+
+    logging.basicConfig(
+        level=log_level,
+        handlers=log_handlers,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
 
 
 def view_image_location_on_map(coordinates):
-    lat, lon = coordinates
-    data_folder = Path('__file__').parents[1]
+    latitude, longitude = coordinates
+    data_folder = Path('__file__').parents[1] / 'logs'
     data_folder.mkdir(exist_ok=True)
 
-    gmap = gmplot.GoogleMapPlotter(lat, lon, 12)
-    gmap.marker(lat, lon, 'cornflowerblue')
-    gmap.draw(data_folder / 'location.html')
+    gmap = gmplot.GoogleMapPlotter(latitude, longitude, 12)
+    gmap.marker(latitude, longitude, 'cornflowerblue')
+    gmap.draw(str(data_folder) + 'location.html')
 
     webbrowser.open(str(data_folder) + 'location.html')
 
@@ -36,8 +46,9 @@ class ImageInfo:
         self.get_metadata()
 
     def get_metadata(self):
-        with pyexiv2.Image(self.image_path) as image_info:
-            self.metadata = image_info.read_exif()
+        with open(self.image_path, 'rb') as f:
+            with pyexiv2.ImageData(f.read()) as image_info:
+                self.metadata = image_info.read_exif()
 
     @property
     def device_model(self):
@@ -67,12 +78,12 @@ class ImageInfo:
     @property
     def coordinates(self):
         if 'Exif.GPSInfo.GPSLatitude' in self.metadata:
-            latitude = self.metadata['Exif.GPSInfo.GPSLatitude']
-            longitude = self.metadata['Exif.GPSInfo.GPSLongitude']
+            latitude_data = self.metadata['Exif.GPSInfo.GPSLatitude']
+            longitude_data = self.metadata['Exif.GPSInfo.GPSLongitude']
 
-            latitude = [eval(values) for values in latitude.split()]
+            latitude = [eval(values) for values in latitude_data.split()]
             latitude_deg = latitude[0] + latitude[1] / 60 + latitude[2] / 3600
-            longitude = [eval(values) for values in longitude.split()]
+            longitude = [eval(values) for values in longitude_data.split()]
             longitude_deg = longitude[0] + longitude[1] / 60 + longitude[2] / 3600
 
             return latitude_deg, longitude_deg
@@ -86,10 +97,10 @@ class ImageInfo:
                 location = geolocator.reverse((latitude, longitude), language='en')
                 return location.address
             except GeocoderTimedOut:
-                logging.error("Geocoding service timed out. Skipping address retrieval.")
+                logging.error("Geocoding service timed out.")
                 return None
             except GeocoderUnavailable:
-                logging.error("Geocoding service unavailable. Skipping address retrieval.")
+                logging.error("Geocoding service unavailable.")
                 return None
             except Exception as e:
                 logging.error(f"Error during geocoding: {e}")
@@ -101,39 +112,54 @@ class ImageInfo:
 class ImageViewer:
     def __init__(self, master):
         self.master = master
-        self.master.geometry('400x400')
-        self.master.title("My Image Viewer")
+        self.master.geometry('500x550')
+        self.master.title("Image Viewer")
 
         self.image_path = None
         self.image = None
-        self.image_label = Label(self.master)
-        self.image_label.pack()
-
-        self.load_button = Button(self.master, text="Open Image", command=self.load_image)
+        # widgets
+        self.load_button = ttk.Button(self.master, text="Open Image", command=self.load_image)
+        self.info_textbox = tk.Text(self.master, wrap="word", height=5, width=40)
+        self.image_label = ttk.Label(self.master)
+        # layout
         self.load_button.pack()
+        self.info_textbox.pack()
+        self.image_label.pack()
 
     def load_image(self):
         file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
-        if is_image(file_path):
+        if file_path:
             self.image_path = file_path
-        self.info = ImageInfo(self.image_path)
-        self.display_image()
+            self.info = ImageInfo(self.image_path)
+            self.display_image()
+
+    def resize(self, image):
+        original_width, original_height = image.size
+        aspect_ratio = original_width / original_height
+        new_height = int(150 / aspect_ratio)
+        image = image.resize((150, new_height), Image.Resampling.LANCZOS)
+
+        return image
 
     def display_image(self):
         if self.image_path:
             image = Image.open(self.image_path)
-            # TODO: image should retain its aspect ratio, impliment logic to view infos in app.
-            image = image.resize((300, 300), Image.Resampling.LANCZOS)
+            image = self.resize(image)
             self.image = ImageTk.PhotoImage(image)
             self.image_label.config(image=self.image)
-            self.image_label.image = self.image
-            print(f'date taken: {self.info.date_taken}')
-            print(f'coordinates: {self.info.coordinates}')
-            print(f'make of device took: {self.info.device_make}')
-            print(f'model of device took: {self.info.device_model}')
-            print(f'address where image was taken: {self.info.address}')
+
+            info_text = (
+                f'date taken: {self.info.date_taken}\n'
+                f'coordinates: {self.info.coordinates}\n'
+                f'make of device took: {self.info.device_make}\n'
+                f'model of device took: {self.info.device_model}\n'
+                f'address where image was taken: {self.info.address}'
+            )
+            self.info_textbox.delete(1.0, "end")  # Clear previous text
+            self.info_textbox.insert("end", info_text)
 
 
 if __name__ == "__main__":
-    app = ImageViewer(Tk())
+    configure_logging()
+    app = ImageViewer(ttk.Window(themename='darkly'))
     app.master.mainloop()
