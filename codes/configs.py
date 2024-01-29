1eb23403e731278e26ad1432996f6f95d501d223
Kenneth <kennethalisen433@protonmail.com>
2024-01-29 14:40:44 +0800

    configs.py - created a logger for other modules for a centralized logging.

--- codes/configs.py
+++ codes/configs.py
@@ -7,6 +7,8 @@ data_folder.mkdir(exist_ok=True)
 logs_folder = Path(__file__).parents[1] / 'logs'
 logs_folder.mkdir(exist_ok=True)
 
+logger = logging.getLogger(__name__)
+
 
 def configure_logging(*, log_level=logging.ERROR, log_file=None):
     log_handlers = [logging.StreamHandler()]
